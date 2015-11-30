d7111180bc4a46a5255df85ca7746d32ffc0c294
sabbisun <sabbisun@gmail.com>
2015-11-30 10:04:09 +0000

    breytti menu

--- program/main.cpp
+++ program/main.cpp
@@ -37,9 +37,9 @@ bool legalAns(char ans) {
 void ask() {
     cout << "Do you want to: " << endl;
     cout << "(A) Add to list? " << endl;
-    cout << "(B) Search list? " << endl;
-    cout << "(C) Print list? " << endl; //fyrst spurt um sort svo print
-    cout << "(D) Remove from list? " << endl;
+    cout << "(S) Search list? " << endl;
+    cout << "(P) Print list? " << endl; //fyrst spurt um sort svo print
+    cout << "(R) Remove from list? " << endl;
 }
 
 void menu(char ans, People& list)
@@ -53,16 +53,16 @@ void menu(char ans, People& list)
                         list.addIndi(id);
                     }
                     break;
-        case 'b':
-        case 'B':   searchMenu(list);
+        case 's':
+        case 'S':   searchMenu(list);
                     break;
-        case 'c':
-        case 'C':   {
+        case 'p':
+        case 'P':   {
                         sortMenu(list);
                     }
                     break;
-        case 'd':
-        case 'D':   list = list.removeIndi(list);
+        case 'r':
+        case 'R':   list = list.removeIndi(list);
                     break;
         default:;
     }
